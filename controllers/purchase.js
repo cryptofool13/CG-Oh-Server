d6596002603d605b5e3d7025e8d9750cd91093a2
michaelcurry95 <fender18295@gmail.com>
2019-12-28 21:26:09 -0500

    purchaseItems controller

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -1,22 +1,34 @@
 const db = require("../db")
-const { isValidUpc } = require("../helpers/util")
+const { isValidUpc, flattenItems } = require("../helpers/util")
 
 module.exports = {}
 
 // generate an update query to insert into transaction
 // throw error if upc is invalid or if ct variable is not an Int > 0
-function updateItem({ upc, ct }) {
-	if (!isValidUpc(upc)) {
-		throw Error(`invalid upc: ${upc}`)
+function updateItem(ct, upc) {
+	// TODO: move V this logic to POS api V
+	// if (!isValidUpc(upc)) {
+	// 	throw Error(`invalid upc: ${upc}`)
+	// }
+	// if (ct < 1 || !Number.isInteger(ct)) {
+	// 	throw Error(`invalid count: ${ct}`)
+	// }
+	return `UPDATE items SET on_hand = on_hand - ${ct} WHERE upc = '${upc}';`
+}
+
+function buildTransaction(items) {
+	let query = ""
+	for (let i = 0; i < items.length; i += 2) {
+		let ct = `$${i + 1}`
+		let upc = `$${i + 2}`
+		query += updateItem(ct, upc)
 	}
-	if (ct < 1 || !Number.isInteger(ct)) {
-		throw Error(`invalid count: ${ct}`)
-	}
-	return `UPDATE items SET on_hand = on_hand - ${ct} WHERE upc = '${upc}'`
+	return query
 }
 
 // assume req.body holds array of {upc, ct}
 function purchaseItems(req, res) {
+	const items = flattenItems(req.body.items)
 	db.pool.connect((err, client, done) => {
 		const shouldAbort = err => {
 			if (err) {
@@ -32,25 +44,23 @@ function purchaseItems(req, res) {
 			return !!err
 		}
 
-		client.query('BEGIN', err => {
-			if(shouldAbort(err)) {
-				res.status(500).json({error: err})
+		client.query("BEGIN", err => {
+			if (shouldAbort(err)) {
+				res.status(500).json({ error: err })
 			}
-			
+			const queryText = buildTransaction(items)
+			client.query(queryText, items, (err, result) => {
+				if (shouldAbort(err)) {
+					res.status(500).json({ error: err })
+				}
+
+				client.query('COMMIT', err => {
+					if(err) {
+						res.status(500).json({ error: err })
+					}
+					 done()
+				})
+			})
 		})
 	})
 }
-// const shouldAbort = err => {
-//   if (err) {
-//     console.error('Error in transaction', err.stack)
-//     client.query('ROLLBACK', err => {
-//       if (err) {
-//         console.error('Error rolling back client', err.stack)
-//       }
-//       // release the client back to the pool
-//       done()
-//     })
-//   }
-//   return !!err
-// }
-console.log(updateItem({ id: "121212121212", ct: 14 }))
